538198c75bdcf53da6a4e11472c51a4e4b284a95
Matvey <ushiz.rush@gmail.com>
2025-03-29 11:39:34 +0300

    update mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,56 @@
 return {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup({})
-  end,
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          border = "rounded",
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "",
+            package_pending = "",
+            package_uninstalled = "",
+          },
+          border = "rounded",
+        },
+      })
+
+      require("mason-lspconfig").setup({
+        ensure_installed = {},
+        automatic_installation = true,
+      })
+
+
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          require("lspconfig")[server_name].setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            settings = {
+              Lua = {
+                diagnostics = { globals = { "vim" } },
+              },
+            },
+          })
+        end,
+      })
+    end,
+  }
 }
